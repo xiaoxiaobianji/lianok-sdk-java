1687503858b8f7876fa512d86b20f1946abe9fcf
zh <huangzh@guahao.com>
2023-11-22 10:45:20 +0800

    chiper

---
+++ src/main/java/com/lianok/docking/notification/ComplaintCiphertextNotification.java
@@ -0,0 +1,14 @@
+package com.lianok.docking.notification;
+
+public class ComplaintCiphertextNotification {
+
+    private String ciphertext;
+
+    public String getCiphertext() {
+        return ciphertext;
+    }
+
+    public void setCiphertext(String ciphertext) {
+        this.ciphertext = ciphertext;
+    }
+}
