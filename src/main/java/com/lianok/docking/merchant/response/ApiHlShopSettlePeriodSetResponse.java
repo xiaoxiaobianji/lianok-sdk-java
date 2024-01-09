9eb56f53b54d98ea1008ddfe75eaa6b0d44e70f0
zh <huangzh@guahao.com>
2024-01-09 11:34:31 +0800

    增加 分时结算开通 和 查询分时开通信息查询接口

---
+++ src/main/java/com/lianok/docking/merchant/response/ApiHlShopSettlePeriodSetResponse.java
@@ -0,0 +1,33 @@
+package com.lianok.docking.merchant.response;
+
+import com.lianok.core.entity.DockingResponseBase;
+
+
+public class ApiHlShopSettlePeriodSetResponse extends DockingResponseBase {
+
+    /**
+     * 分时结算周期信息
+     */
+    private ApiHlShopSettlePeriodUsedResponseVO settlePeriodInfo;
+
+    /**
+     * 申请变更的信息
+     */
+    private ApiHlShopSettlePeriodApplyResponseVO applyChangeInfo;
+
+    public ApiHlShopSettlePeriodUsedResponseVO getSettlePeriodInfo() {
+        return settlePeriodInfo;
+    }
+
+    public void setSettlePeriodInfo(ApiHlShopSettlePeriodUsedResponseVO settlePeriodInfo) {
+        this.settlePeriodInfo = settlePeriodInfo;
+    }
+
+    public ApiHlShopSettlePeriodApplyResponseVO getApplyChangeInfo() {
+        return applyChangeInfo;
+    }
+
+    public void setApplyChangeInfo(ApiHlShopSettlePeriodApplyResponseVO applyChangeInfo) {
+        this.applyChangeInfo = applyChangeInfo;
+    }
+}
